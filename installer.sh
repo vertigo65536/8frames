b6469f3879ddcd0f8f6519a939ac44daedd8ca1f
david <david.the.nerd1@gmail.com>
2020-11-29 18:23:32 +0000

    updated installer to be more universal

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pip install fuzzy-match --user
-pip install numpy --user
-pip install tabulate --user
+pip install fuzzy-match --user || pip3 install fuzzy-match --user
+pip install numpy --user || pip3 install numpy --user
+pip install tabulate --user || pip3 install tabulate --user
 
 git clone https://github.com/D4RKONION/fatsfvframedatajson.git
