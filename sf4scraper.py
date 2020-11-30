59b93002153a1a54fa98d9e8ca379fe6a72c521c
david <david.the.nerd1@gmail.com>
2020-11-30 15:40:23 +0000

    mostly working version of sf4scraper, which also pulls move inputs

--- sf4scraper.py
+++ sf4scraper.py
@@ -18,7 +18,7 @@ def convertHtmlTableToArray(soup):
         for td in tr.findAll("td"):
             if 'colspan' in td.attrs:
                 break
-            tempArray.append(td.getText().rstrip())
+            tempArray.append(parseTableEntry(td))
         if tempArray == []:
             continue
         array.append(tempArray)
@@ -30,6 +30,94 @@ def getTableHeader(soup):
         for th in tr.findAll("th"):
             array.append(th.getText())
         return array
+
+def parseTableEntry(soup):
+    for a in soup.findAll("a"):
+        link = a['href'].split("/")
+        link = link[len(link) - 1].split(":")
+        link = link[len(link) - 1].split(".")
+        link = link[0]
+        a.contents[0].replaceWith(link)
+    return soup.getText().rstrip()
+
+def removeButton(string):
+    string = string.replace(" Armorbreak", "")
+    string = string.replace("P Ex", "P")
+    string = string.replace("K Ex", "K")
+    buttons = {
+        ' Jab': "Lp",
+        ' Strong': "Mp",
+        ' Fierce': "Hp",
+        ' Short': "Lk",
+        ' Forward': "Mk",
+        ' Roundhouse': "Hk",
+        ' EX': "Ex",
+        ' 3p': "3p",
+        ' 3k': "3k"}
+    for key, value in buttons.items():
+        string = string.replace(value + " Ex", value)
+        if key in string:
+            return [string.replace(key, ""), value]
+    return [string, ""]
+
+def getMoveType(string, soup):
+    td = soup.find("td", text=string)
+    td = td.findNext("td")
+    move = parseTableEntry(td).lower()
+    if any(substring in move for substring in ['jab', 'strong', 'fierce']):
+        return 'P'
+    elif any(substring in move for substring in ['short', 'forward', 'roundhouse']):
+        return 'K'
+    return -1
+
+def findSpecialCommand(string, soup):
+    buttonParse = removeButton(string)
+    string = buttonParse[0]
+    button = buttonParse[1]
+    for td in soup.findAll("td", text=string):
+        nextTd = td.findNext("td")
+        #print(">" + nextTd.getText().lower().rstrip() + "<")
+        if nextTd.getText().lower().rstrip() == string.lower().rstrip():
+            nextTd = nextTd.findNext("td")
+        elif nextTd.getText().lower().rstrip() == "-":
+            nextTd = nextTd.findNext("td")
+        else:
+            try:
+                int(nextTd.getText().lower().rstrip())
+                continue
+            except:
+                pass
+            if string.lower().rstrip() in nextTd.getText().lower().rstrip():
+                continue
+        moveInput = removeButton(parseTableEntry(nextTd))[0]
+        if button == "Ex":
+            moveInput = moveInput.replace("+ K", "+ 2K")
+            moveInput = moveInput.replace("+ P", "+ 2P")
+        else:
+            moveInput = moveInput.replace("+ K", "+ " + button)
+            moveInput = moveInput.replace("+ P", "+ " + button)
+        return moveInput
+    return "couldn't find move"
+
+def findMoveCommand(string, soup):
+    string = string.replace("Ultra Combo 1", "Ultra Combo I")
+    string = string.replace("Ultra Combo 2", "Ultra Combo II")
+    string = string.replace("Angled", "Diagonal")
+    string = string + "\n"
+    if "Red Focus" in string:
+        return "Lp + Mp + Mk"
+    for td in soup.findAll("td", text=string):
+        nextTd = td.findNext("td")
+        if nextTd.getText().lower().rstrip() == string.lower().rstrip():
+            nextTd = nextTd.findNext("td")
+        else:
+            try:
+                int(nextTd.getText().lower().rstrip())
+                continue
+            except:
+                pass
+        return parseTableEntry(nextTd).replace(" Armorbreak", "")
+    return findSpecialCommand(string, soup)
 
 characterLinks = {}
 folder = getPath("sf4")
@@ -74,9 +162,11 @@ for character, link in characterLinks.items():
     table = soup.find("h2", text="Frame Data (At A Glance)").findNext("table")
     tableArray = convertHtmlTableToArray(table)
     #headers = getTableHeader(table)
-    try:
-        with open(os.path.join(folder, character + ".csv"), 'a') as csv_file:
-            for i in range(len(tableArray)):
-                csv_file.write('`'.join(tableArray[i]) + "\n")
-    except:
-        print("Failed to open " + os.path.join(folder, character + ".csv"))
+    #try:
+    with open(os.path.join(folder, character + ".csv"), 'a') as csv_file:
+        for i in range(len(tableArray)):
+            print(tableArray[i][1] + " _ " + str(findMoveCommand(tableArray[i][1], soup)))
+            csv_file.write('`'.join(tableArray[i]) + "\n")
+    #except:
+    #    print("Failed to open " + os.path.join(folder, character + ".csv"))
+    break
