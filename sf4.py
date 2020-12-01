f350d21e89a5167bd82469b2ca0e886f5bdf606f
david <david.the.nerd1@gmail.com>
2020-12-01 02:49:25 +0000

    improved sf4 searching, added punishable function

--- sf4.py
+++ sf4.py
@@ -1,11 +1,11 @@
 import tools
-import os, discord, string, csv
+import os, discord, string, csv, re
 from fuzzy_match import match, algorithims
 from tabulate import tabulate
 
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sf4")
 
-headers = [
+titles = [
     'Input',
     'Move Name',
     'Nickname',
@@ -23,14 +23,18 @@ headers = [
 
 def parseCommand(command):
     character = tools.getMessagePrefix(command)
-    content = tools.getMessageContent(command)
+    content = translateAcronym(tools.getMessageContent(command))
     files = os.listdir(path)
 
     fuzzyMatch  = match.extractOne(character + ".csv", files)
     if fuzzyMatch[1] < 0.6:
         return "Could not find character '" + character + "'"
     file = path + "/" + fuzzyMatch[0]
-
+    character = fuzzyMatch[0].replace(".csv", "")
+    if content.lower() == "punishable":
+        return getPunishable(file, character, 1)
+    if content.lower() == "lose turn":
+        return getPunishable(file, character, 0)
     searchOutput = []
     searchOutput.append(getMoveByCommand(content, file))
     searchOutput.append(getMoveByName(content, file))
@@ -39,7 +43,7 @@ def parseCommand(command):
     for i in range(len(searchOutput)):
         if searchOutput[i][1] > outputValue[1]:
             outputValue = searchOutput[i]
-    return getMoveEmbed(outputValue[0], fuzzyMatch[0].replace(".csv", ""))
+    return getMoveEmbed(outputValue[0], character)
 
 def getMoveByCommand(id, file):
     return getMoveByNthKey(id, file, 0)
@@ -76,10 +80,56 @@ def getMoveEmbed(moveRow, character):
     e = discord.Embed(title=character)
     for i in range(len(moveRow)):
         e.add_field(
-            name = headers[i],
+            name = titles[i],
             value = moveRow[i]
         )
     return e
 
+def getPunishable(file, character, punishable = 0):
+    with open(file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='`')
+        moves = []
+        minimum = 0
+        maximum = 0
+        oBKey = 11
+        if punishable == 0:
+            minimum = -1
+            maximum = -2
+        elif punishable == 1:
+            minimum = -3
+            maximum = -200
+        else:
+           return -1
+        for row in csv_reader:
+            print(row)
+            if len(row) <= 0 or removePunctuation(row[oBKey]) in ["", None]:
+                continue
+            oB = row[oBKey]
+            if int(oB) <= minimum and int(oB) >= maximum:
+                    moves.append(row)
+    e = discord.Embed(title=character)
+    headers = [titles[0], titles[1], titles[oBKey]]
+    embedArray = []
+    offset = 0
+    finished = 0
+    listSize = 40
+    while(True):
+        stringArray = []
+        for i in range(offset, offset + listSize - 1):
+            if i >= len(moves):
+                finished = 1
+                break
+            stringArray.append([moves[i][0], moves[i][1], moves[i][oBKey]])
+        embedArray.append("```" + tabulate(stringArray, headers=headers) + "```")
+        offset += listSize
+        if finished == 1:
+            break
+    return embedArray
+ 
+def translateAcronym(text):
+    if re.match(r"tc[0-9]+", text.lower()):
+        text = text.replace("tc", "Target Combo ")
+    return text
+
 def removePunctuation(text):
-    return text.translate(str.maketrans('', '', string.punctuation)).rstrip().replace("+", "").lower()
+    return text.translate(str.maketrans('', '', string.punctuation)).rstrip().replace("+", "").lower().replace(" ", "")
