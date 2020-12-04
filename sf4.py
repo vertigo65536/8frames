3722d11c0bae4b0d171af6cb0b74a80acb1ea6a7
david <david.the.nerd1@gmail.com>
2020-12-04 01:46:47 +0000

    converted sf4 to use json. Added sf4scrapejson.py

--- sf4.py
+++ sf4.py
@@ -1,5 +1,5 @@
 import tools
-import os, discord, string, csv, re
+import os, discord, string, re, json
 from fuzzy_match import match, algorithims
 from tabulate import tabulate
 
@@ -40,75 +40,78 @@ def parseCommand(command):
     character = tools.getMessagePrefix(command)
     if character.lower() == "gen":
         return "Use GenMantis or GenCrane"
-    content = translateAcronym(tools.getMessageContent(command))
+    content = translateAcronym(tools.getMessageContent(command)).replace(" ", "")
     files = os.listdir(path)
 
-    fuzzyMatch  = match.extractOne(character + ".csv", files)
+    fuzzyMatch  = match.extractOne(character + ".json", files)
     if fuzzyMatch[1] < 0.6:
         return "Could not find character '" + character + "'"
     file = path + "/" + fuzzyMatch[0]
-    character = fuzzyMatch[0].replace(".csv", "")
+    character = fuzzyMatch[0].replace(".json", "")
     if content.lower() == "punishable":
         return getPunishable(file, character, 1)
-    if content.lower() == "lose turn":
+    if content.lower() == "loseturn":
         return getPunishable(file, character, 0)
     searchOutput = []
-    searchOutput.append(getMoveByCommand(content, file))
+    if "genei" not in content:
+        searchOutput.append(getMoveByValue(content, file, "Input"))
     searchOutput.append(getMoveByName(content, file))
-    searchOutput.append(getMoveByNickname(id, file))
+    searchOutput.append(getMoveByValue(content, file, "Nickname"))
     outputValue = searchOutput[0]
     for i in range(len(searchOutput)):
-        if searchOutput[i][1] > outputValue[1]:
+        if searchOutput[i][2] > outputValue[2]:
             outputValue = searchOutput[i]
-    return getMoveEmbed(outputValue[0], character)
+    return getMoveEmbed(outputValue[0], outputValue[1], character)
 
-def getMoveByCommand(id, file):
-    return getMoveByNthKey(id, file, 0)
-
-def getMoveByName(id, file):
-    return getMoveByNthKey(id, file, 1)
-
-def getMoveByNickname(id, file):
-    return getMoveByNthKey(id, file, 2)
-
-def getMoveByNthKey(id, file, n):
+def getMoveByName(query, f):
     try:
-        with open(file) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter='`')
-            line_count = 0
-            id = removePunctuation(str(id))
-            moveData = {}
-            moveKeys = []
-            counter = 0
-            for row in csv_reader:
-                if len(row) <= 0:
+        with open(f) as json_file:
+            moveList = json.load(json_file)
+            keyList = {}
+            keyArray = []
+            for key, row in moveList.items():
+                if "genei" not in query.lower() and "genei" in key.lower():
                     continue
-                moveKeys.append(removePunctuation(row[n]))
-                moveData[moveKeys[counter]] = row
-                #if moveKeys[counter] == id:
-                #    return row
-                counter += 1
-            matchOutput = match.extractOne(id, moveKeys)
-            return [moveData[matchOutput[0]], matchOutput[1]]
+                keyArray.append(tools.removePunctuation(key))
+                keyList[tools.removePunctuation(key)] = key
+            fuzzyMatch  = match.extractOne(query, keyArray)
+            return [moveList[keyList[fuzzyMatch[0]]], keyList[fuzzyMatch[0]], fuzzyMatch[1]]
+    except:
+        return -1
+   
+
+def getMoveByValue(query, f, moveId):
+    try:
+        with open(f) as json_file:
+            moveList = json.load(json_file)
+            keyList = {}
+            keyArray = []
+            for key, row in moveList.items():
+                if "genei" not in query.lower() and "genei" in key.lower():
+                    continue
+                keyArray.append(tools.removePunctuation(row[moveId]))
+                keyList[tools.removePunctuation(row[moveId])] = key
+            fuzzyMatch  = match.extractOne(query, keyArray)
+            return [moveList[keyList[fuzzyMatch[0]]], keyList[fuzzyMatch[0]], fuzzyMatch[1]]
     except:
         return -1
 
-def getMoveEmbed(moveRow, character):
+def getMoveEmbed(moveRow, moveName, character):
     e = discord.Embed(title=character)
-    for i in range(len(moveRow)):
+    e.add_field(name = "Name", value = moveName)
+    for title, value in moveRow.items():
         e.add_field(
-            name = titles[i],
-            value = moveRow[i]
+            name = title,
+            value = value
         )
     return e
 
-def getPunishable(file, character, punishable = 0):
-    with open(file) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter='`')
+def getPunishable(f, character, punishable = 0):
+    with open(f) as json_file:
+        moveList = json.load(json_file)
         moves = []
         minimum = 0
         maximum = 0
-        oBKey = 11
         if punishable == 0:
             minimum = -1
             maximum = -2
@@ -117,33 +120,32 @@ def getPunishable(file, character, punishable = 0):
             maximum = -200
         else:
            return -1
-        for row in csv_reader:
-            if len(row) <= 0 or removePunctuation(row[oBKey]) in ["", None]:
-                continue
-            oB = row[oBKey]
+        oBHeader = 'Adv OnGuard'
+        for key, move in moveList.items():
+            oB = move[oBHeader]
             oB = oB.split("[")
             for i in range(len(oB)):
+                try:
+                    int(oB[i])
+                except:
+                    continue
                 oB[i].replace("]", "")
                 if int(oB[i]) <= minimum and int(oB[i]) >= maximum:
-                    moves.append(row)
+                    moves.append([key, move[oBHeader]])
                     break
     e = discord.Embed(title=character)
-    headers = [titles[1], titles[oBKey]]
+    headers = ['Name', oBHeader]
     embedArray = []
     offset = 0
     finished = 0
-    listSize = 40
+    listSize = 41
     while(True):
         stringArray = []
-        for i in range(offset, offset + listSize - 1):
+        for i in range(offset, offset + listSize):
             if i >= len(moves):
                 finished = 1
                 break
-            moveName = moves[i][1]
-            moveButton = getButton(moves[i][2])
-            if getButton(moveName) == None and moveButton != None:
-                moveName = moveButton + " " + moveName
-            stringArray.append([moveName, moves[i][oBKey]])
+            stringArray.append([moves[i][0], moves[i][1]])
         embedArray.append("```" + tabulate(stringArray, headers=headers) + "```")
         offset += listSize
         if finished == 1:
@@ -151,8 +153,12 @@ def getPunishable(file, character, punishable = 0):
     return embedArray
  
 def translateAcronym(text):
-    if re.match(r"tc[0-9]+", text.lower()):
-        text = text.replace("tc", "Target Combo ")
+    text = str(text).lower()
+    if re.match(r"tc[0-9]+", text):
+        text = text.replace("tc", "target combo ")
+    if re.match("rekka [0-9]+", text) or  re.match(r"rekkaken [0-9]+", text):
+        text = text.replace("rekka", "rekka part")
+        text = text.replace("part 1", "")
     return text
 
 def removePunctuation(text):
