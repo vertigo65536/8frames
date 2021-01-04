eb7a5e324afe4d551e0d63027e68ffdcdc8d7ac8
david <david.the.nerd1@gmail.com>
2021-01-04 06:48:55 +0000

    added discord py to installer

--- installer.sh
+++ installer.sh
@@ -6,5 +6,7 @@ pip install numpy --user || pip3 install numpy --user
 pip install tabulate --user || pip3 install tabulate --user
 pip install word2number --user || pip3 install word2number --user
 pip install google_trans_new --user || pip3 install google_trans_new --user
+pip install discord --user || pip3 install discord --user
+
 
 git clone https://github.com/D4RKONION/fatsfvframedatajson.git
